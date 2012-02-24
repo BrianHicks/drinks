5b9fb0c0b5d5dd94a2a6d1553fdf500e3f8b353c
Brian Hicks <brian@brianthicks.com>
2012-02-23 18:10:05 -0600

    enable admin app

--- drinks/settings.py
+++ drinks/settings.py
@@ -98,17 +98,18 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
+    # django
     'django.contrib.auth',
+    'django.contrib.admin',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
 
+    # local apps
+
+    # third-party
     'gunicorn',
     'south',
 )
